4441e1a087f356b736af88e679ff0da5f429d302
Jack Kelley <jack.kelley1201@gmail.com>
2024-03-18 15:31:51 -0400

    Work on 3/18/2024

--- plot.py
+++ plot.py
@@ -20,7 +20,7 @@ def set_object_file():
     Sets the file to be used.
     TODO - make this take an input so the GUI can accept a file input
     """
-    file = '108_11_03_26_31_41_15..txt'
+    file = '106_14_41_51_14_20_50..txt'
     return file
 
 
@@ -32,20 +32,92 @@ def parse_shooter_number():
     return file[:-5]
 
 
-def get_doublet_dict(file='line_list.txt'):
+def pick_color():
+    plt.rcParams['axes.prop_cycle'] = plt.cycler(color=['magenta', 'green', 'brown', 'orange', 'darkred', 'goldenrod'])
+    prop_cycle = plt.rcParams['axes.prop_cycle']
+    colors = prop_cycle.by_key()['color']
+    colors = enumerate(colors)
+    return colors
+
+
+def plot_systems(colors, ax, data, matched_mgii=None, matched_civ=None, theoretical_locations=None):
     """
-    :param file: 3 column data file with doublet distances
-    :return: dataframe with doublet data
+    :param colors: the plot colors enumerator
+    :param matched_mgii: MgII doublets
+    :param ax: the plot axes
+    :param data: the quasar data
+    :param matched_civ: CIV doublets
+    :param theoretical_locations: theoretical extrapolated locations of other doublets
+    :return:
     """
-    doublets = pd.read_csv(file, header=None, delim_whitespace=True)
-    doublets.columns = ('Doublet', 'Wavelength', 'Unused')
+    if matched_mgii:
+        for mgii_z in matched_mgii:
+            color = next(colors)[1]
+
+            ax.vlines(data['Observed Wavelength'].loc[matched_mgii[mgii_z][0]], 0, data['Flux'].max(), color=color,
+                      linewidth=0.6, label=str(np.round(mgii_z, decimals=3)) + " MgII")
+            # plot red
+            ax.vlines(data['Observed Wavelength'].loc[matched_mgii[mgii_z][1]], 0, data['Flux'].max(), color=color,
+                      linewidth=0.6)
+
+            # annotate
+            ax.annotate(str(np.round(mgii_z, decimals=3)) + "MgII",
+                        xy=(data['Observed Wavelength'].loc[matched_mgii[mgii_z][0]], 18),
+                        xytext=(data['Observed Wavelength'].loc[matched_mgii[mgii_z][1]] + 5, 18),
+                        rotation=270)
+
+            if theoretical_locations:
+                for theoretical_doublet in theoretical_locations[mgii_z]:
+                    ax.vlines(data['Observed Wavelength'].loc[theoretical_doublet[0]], 0, data['Flux'].max(),
+                              color=color, linewidth=0.6,
+                              )
+                    # plot red
+                    ax.vlines(data['Observed Wavelength'].loc[theoretical_doublet[1]], 0, data['Flux'].max(),
+                              color=color,
+                              linewidth=0.6)
+
+                    ax.annotate(str(np.round(mgii_z, decimals=3)) + " " + theoretical_doublet[2] + " Unconfirmed",
+                                xy=(data['Observed Wavelength'].loc[theoretical_doublet[1]], 18),
+                                xytext=(data['Observed Wavelength'].loc[theoretical_doublet[1]] + 5, 18),
+                                rotation=270)
+    if matched_civ:
+        for civ_z in matched_civ:
+            color = next(colors)[1]
+
+            ax.vlines(data['Observed Wavelength'].loc[matched_civ[civ_z][0]], 0, data['Flux'].max(), color=color,
+                      linewidth=0.6, label=str(np.round(civ_z, decimals=3)) + " CIV")
+            # plot red
+            ax.vlines(data['Observed Wavelength'].loc[matched_civ[civ_z][1]], 0, data['Flux'].max(), color=color,
+                      linewidth=0.6)
+
+            # annotate
+            ax.annotate(str(np.round(civ_z, decimals=3)) + " CIV",
+                        xy=(data['Observed Wavelength'].loc[matched_civ[civ_z][0]], 18),
+                        xytext=(data['Observed Wavelength'].loc[matched_civ[civ_z][1]] + 5, 25),
+                        rotation=270)
+
+            if theoretical_locations:
+                for theoretical_doublet in theoretical_locations[civ_z]:
+                    ax.vlines(data['Observed Wavelength'].loc[theoretical_doublet[0]], 0, data['Flux'].max(),
+                              color=color, linewidth=0.6,
+                              )
+                    # plot red
+                    ax.vlines(data['Observed Wavelength'].loc[theoretical_doublet[1]], 0, data['Flux'].max(),
+                              color=color,
+                              linewidth=0.6)
+
+                    ax.annotate(str(np.round(civ_z, decimals=3)) + " " + theoretical_doublet[2] + " Unconfirmed",
+                                xy=(data['Observed Wavelength'].loc[theoretical_doublet[1]], 18),
+                                xytext=(data['Observed Wavelength'].loc[theoretical_doublet[1]] + 5, 18),
+                                rotation=270)
 
 
 def plot_object():
     """
     Creates plot from text data file.
     """
-    data = pd.read_csv('108_11_03_26_31_41_15..txt', header=None, delim_whitespace=True)
+    colors = pick_color()
+    data = pd.read_csv('106_14_41_51_14_20_50..txt', header=None, delim_whitespace=True)
     data.columns = ('Observed Wavelength', 'Flux', 'Flux Error')
     # get all redshifts
     all_redshifts = pd.read_csv('Object_Index.txt', header=None)
@@ -66,7 +138,9 @@ def plot_object():
 
     possible_intervening_redshifts = trough_identify.determine_possible_redshifts(data, troughs[0])
     matched_mgii = trough_identify.match_mgii(data, troughs[0], possible_intervening_redshifts)
-    theoretical_locations = trough_identify.theoretical_doublets(data, matched_mgii)
+    matched_civ = trough_identify.match_civ(data, troughs[0], possible_intervening_redshifts)
+    theoretical_locations_mgii = trough_identify.theoretical_doublets(data, matched_mgii, "MgII")
+    theoretical_locations_civ = trough_identify.theoretical_doublets(data, matched_civ, "CIV")
 
     # create figure and axis
     fig, ax = plt.subplots()
@@ -74,7 +148,7 @@ def plot_object():
     plt.tight_layout()
 
     # plot flux and error
-    ax.step(data['Observed Wavelength'], data['Flux'], linewidth=0.5, where="mid")
+    ax.step(data['Observed Wavelength'], data['Flux'], linewidth=0.5, where="mid", color='#1f77b4')
     ax.step(data['Observed Wavelength'], data['Flux Error'], linewidth=1, color='red', where="mid")
 
     # rest wave axis on top
@@ -85,24 +159,8 @@ def plot_object():
     # ax.vlines(data['Observed Wavelength'].iloc[troughs[0]], 0, data['Flux'].max(), color='purple')
 
     # plot matched systems
-    for z in matched_mgii:
-        color = np.random.random(3)
-        ax.vlines(data['Observed Wavelength'].loc[matched_mgii[z][0]], 0, data['Flux'].max(), color=color,
-                  linewidth=0.6, label=np.round(z, decimals=3))
-        # plot red
-        ax.vlines(data['Observed Wavelength'].loc[matched_mgii[z][1]], 0, data['Flux'].max(), color=color,
-                  linewidth=0.6)
-
-        for theoretical_doublet in theoretical_locations[z]:
-            ax.vlines(data['Observed Wavelength'].loc[theoretical_doublet[0]], 0, data['Flux'].max(), color=color,
-                      linewidth=0.6, label=(str(np.round(z, decimals=3)) + " " + theoretical_doublet[2]))
-            # plot red
-            ax.vlines(data['Observed Wavelength'].loc[theoretical_doublet[1]], 0, data['Flux'].max(), color=color,
-                      linewidth=0.6)
-
-            ax.annotate(str(np.round(z, decimals=3)) + " " + theoretical_doublet[2] + '*',
-                        xy=(data['Observed Wavelength'].loc[theoretical_doublet[1]], 25),
-                        xytext=(data['Observed Wavelength'].loc[theoretical_doublet[1]] + 5, 25))
+    plot_systems(colors, ax, data, matched_mgii=matched_mgii, theoretical_locations=theoretical_locations_mgii)
+    plot_systems(colors, ax, data, matched_civ=matched_civ, theoretical_locations=theoretical_locations_civ)
 
     # set plot limits
     ax.set_xlim(data['Observed Wavelength'].min(), data['Observed Wavelength'].max())
